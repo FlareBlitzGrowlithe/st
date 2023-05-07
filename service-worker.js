243ed76d0fbee91150053008ebba658d49a309d3
Growlithe <reonaasaka@gmail.com>
2023-05-06 22:16:56 -0700

    init

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'my-app-cache';
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                'icon192.png',
+                "icon512.png"
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(keyList.map(key => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+// Serve cached assets when available
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
